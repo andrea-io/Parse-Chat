7ca9aff08dbad6690a0553846fafddf641e75327
Andrea Gonzalez <andreagonz@fb.com>
2022-06-27 10:12:27 -0700

    Created table view and cell for chat messages

---
+++ Parse Chat/Cells/ChatCell.h
@@ -0,0 +1,18 @@
+//
+//  ChatCell.h
+//  Parse Chat
+//
+//  Created by Andrea Gonzalez on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *chatMessageLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Parse Chat/View Controllers/ChatViewController.m
+++ Parse Chat/View Controllers/ChatViewController.m
@@ -6,20 +6,49 @@
 //
 
 #import "ChatViewController.h"
+#import "ChatCell.h"
+#import <Parse/Parse.h>
 
-@interface ChatViewController ()
+@interface ChatViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *chatTableView;
+@property (nonatomic, strong) NSArray *messages;
 
 @end
 
 @implementation ChatViewController
 
 - (IBAction)sendMessageButton:(id)sender {
+    PFObject *chatMessage = [PFObject objectWithClassName:@"Message_FBU2021"];
     
+    // Retrieve the text message the user typed
+    chatMessage[@"text"] = self.messageTextField.text;
+    
+    // Call saveInBackground(block:) and print when the message successfully saves or produces any errors.
+    [chatMessage saveInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (succeeded) {
+            NSLog(@"The message was saved!");
+            
+            // Clear the text from the text chat field on successful message save
+            chatMessage[@"text"] = @"";
+        } else {
+            NSLog(@"Problem saving message: %@", error.localizedDescription);
+        }
+    }];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.chatTableView.delegate = self;
+    self.chatTableView.dataSource = self;
+    self.chatTableView.rowHeight = UITableViewAutomaticDimension;
+    
+}
+
+- (void)onTimer {
+   // Add code to be run periodically
+    
 }
 
 /*
@@ -31,5 +60,13 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+//- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+//    <#code#>
+//}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.messages.count;
+}
 
 @end
